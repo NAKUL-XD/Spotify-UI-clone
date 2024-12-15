eda5399e4e41a617d54cd24c94af2751de3bf081
NAKUL-XD <nakullegendary@gmail.com>
2024-12-15 23:39:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,55 +20,47 @@ function secondsToMinutesSeconds(seconds) {
 }
 
 async function getSongs(folder) {
-    currFolder = folder; // Set the current folder
-    try {
-        // Fetch the contents of the specified folder
-        let a = await fetch(`./${folder}/`); // Use the correct folder path
-        if (!a.ok) {
-            throw new Error(`Network response was not ok: ${a.statusText}`);
+    currFolder = folder;
+    let a = await fetch(`/${folder}/`)
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    songs = []
+    console.log(songs)
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(`/${folder}/`)[1])
         }
-        
-        let response = await a.text();
-        let div = document.createElement("div");
-        div.innerHTML = response;
-        let as = div.getElementsByTagName("a");
-        songs = [];
-        
-        // Collect all the .mp3 files
-        for (let index = 0; index < as.length; index++) {
-            const element = as[index];
-            if (element.href.endsWith(".mp3")) {
-                // Push the song name to the songs array
-                songs.push(element.href.split(`/${folder}/`)[1]);
-            }
-        }
-
-        // Show all the songs in the playlist
-        let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
-        songUL.innerHTML = "";
-        for (const song of songs) {
-            songUL.innerHTML += `<li><img class="invert" width="34" src="img/music.svg" alt="">
-                                <div class="info">
-                                    <div>${song.replaceAll("%20", " ")}</div>
-                                    <div>Harry</div>
-                                </div>
-                                <div class="playnow">
-                                    <span>Play Now</span>
-                                    <img class="invert" src="img/play.svg" alt="">
-                                </div></li>`;
-        }
-
-        // Attach an event listener to each song
-        Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
-            e.addEventListener("click", element => {
-                playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
-            });
-        });
-
-        return songs; // Return the songs array
-    } catch (error) {
-        console.error(`Failed to fetch songs: ${error.message}`);
     }
+
+
+
+    // Show all the songs in the playlist
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    songUL.innerHTML = ""
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                                <div>Harry</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                               A<img class="invert" src="img/play.svg" alt="">
+                            </div> </li>`;
+    }
+
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+        })
+    })
+
+    return songs
 }
 
 const playMusic = (track, pause = false) => {
