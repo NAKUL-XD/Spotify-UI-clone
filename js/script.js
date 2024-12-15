6f052a121b7715339192d96f98a6e7fd1f2ffcf7
nattyace <nsinghdhanik@gmail.com>
2024-12-15 23:11:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/songs/`)
+    let a = await fetch('./songs/'); // Relative path
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
